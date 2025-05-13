1accabd2feea658eb44709e7dbde79e08d22b803
LiamTuft <liamtuft@gmail.com>
2025-05-13 15:33:10 -0600

    school update today 2
    paul says hi

--- fivenightsgame/fivenightsintheroboticsroom/scripts/camera_system.gd
+++ fivenightsgame/fivenightsintheroboticsroom/scripts/camera_system.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@export var active = false
+
 @onready var originCam = $"../CharacterBody3D/Camera3D"
 @onready var cam1 = $Cam1
 @onready var player = $"../CharacterBody3D"
@@ -12,9 +14,11 @@ func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Ve
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true and inProcess == false and player.state == 2:
 			inProcess = true
 			player.state = -1
+			# animation
+			
 			activeCam.current = true
 			originCam.current = false
-			
+			active = true
 			inProcess = false
 
 # exit button on the top left of the screen
@@ -22,3 +26,8 @@ func _on_exit_button_pressed() -> void:
 	activeCam.current = false
 	originCam.current = true
 	player.state = 2
+	active = false
+
+# use a button system where buttons are togglable and 
+# are disabled after clicking them (reenable disabled buttons)
+# that way they stay looking selected
