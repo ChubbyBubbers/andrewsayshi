1d006095e58b1f2aefa8b6cb3b23f6333c4ae87b
LiamTuft <liamtuft@gmail.com>
2025-05-15 15:31:33 -0600

    camera animations working kinda

--- fivenightsgame/fivenightsintheroboticsroom/scripts/camera_system.gd
+++ fivenightsgame/fivenightsintheroboticsroom/scripts/camera_system.gd
@@ -6,8 +6,13 @@ extends Node3D
 @onready var cam1 = $Cam1
 @onready var player = $"../CharacterBody3D"
 @onready var camObject = $Area3D
-
 @onready var activeCam = $Cam1
+
+var camStartPos = Vector3.ZERO
+var camStartRot = Vector3.ZERO
+var camEndPos = Vector3(0.228,-0.606,-0.1)
+var camEndRot = Vector3(deg_to_rad(67.7),0.0,0.0)
+
 func _ready() -> void:
 	pass
 
@@ -17,6 +22,11 @@ func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Ve
 			player.inProcess = true
 			player.state = -1
 			# animation
+			var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+			tween.set_parallel(true)
+			tween.tween_property(camObject, "position", camEndPos, .5)
+			tween.tween_property(camObject, "rotation", camEndRot, .5)
+			await tween.tween_interval(1).finished
 			
 			activeCam.current = true
 			originCam.current = false
@@ -30,12 +40,15 @@ func _on_exit_button_pressed() -> void:
 	originCam.current = true
 	player.state = 2
 	active = false
+	# animation (FIXX!!!!)
+	var tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	tween.set_parallel(true)
+	tween.tween_property(camObject, "position", camStartPos, .5)
+	tween.tween_property(camObject, "rotation", camStartRot, .5)
+	await tween.tween_interval(1).finished
 	player.inProcess = false
 
-# - use a button system where buttons are togglable and 
-# are disabled after clicking them (reenable disabled buttons)
-# that way they stay looking selected
-# - make all connect to one func
+# - use a button system where all connect to one func
 # - make animation FIRST PLSSSSS
 
 
